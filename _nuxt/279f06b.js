c2ea5acbd43857949f17e50064793d23c9019f11
trinhbiendich <trinhbiendich@users.noreply.github.com>
2021-02-05 08:44:31 +0000

    deploy: 1f1a1c97de9b470b2dd6f038b5daa9ef1dc0254d

--- _nuxt/279f06b.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{214:function(e,n,t){"use strict";t.r(n);var r={name:"add-new"},c=t(27),component=Object(c.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("h1",[this._v("Add new user")])}),[],!1,null,"699e72e3",null);n.default=component.exports}}]);
