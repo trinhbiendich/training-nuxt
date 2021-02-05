f116cc55df86d79c1ab449a6bff7039ab0a54fb9
trinhbiendich <trinhbiendich@users.noreply.github.com>
2021-02-05 15:18:03 +0000

    deploy: 53ffda22b537763d50303fe7eee3a36376a3c00c

--- _nuxt/3864c88.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{253:function(e,t,n){"use strict";n.r(t);var l={name:"detail"},r=n(46),component=Object(r.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("h1",[this._v("Detail user")])}),[],!1,null,"31df3574",null);t.default=component.exports}}]);
