6e8bfa6a743753be33b330fb94b78f6ddccde8d6
Sac Le <le.sinh@mail.i-on.net>
2021-06-12 08:07:53 +0700

    commit property

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { property: 'fb:pages', content: '1441101955956343'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
