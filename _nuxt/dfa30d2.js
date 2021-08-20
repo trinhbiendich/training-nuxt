e7b600f04f5ac082d12999e5d605f24ccf481f4b
trinhbiendich <trinhbiendich@users.noreply.github.com>
2021-08-20 08:43:55 +0000

    deploy: b5088547304babcd55b5a09ebea1a244850e41d8

--- _nuxt/dfa30d2.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{316:function(e,t,n){"use strict";n.r(t);var l={name:"detail"},r=n(49),component=Object(r.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("h1",[this._v("Detail user")])}),[],!1,null,"31df3574",null);t.default=component.exports}}]);
